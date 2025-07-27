998c8a882888a520f6d0c035e02e6015ae814e25
fabbfoto <frankburkert@gmx.net>
2025-07-27 22:19:45 +0200

    🔧 Netlify Build-Fix: fsevents als external hinzugefügt

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,7 @@ export default defineConfig({
       input: {
         main: resolve(__dirname, 'index.html'),
       },
+      external: ['fsevents'],
       output: {
         manualChunks: {
           'vendor': ['vite'],
